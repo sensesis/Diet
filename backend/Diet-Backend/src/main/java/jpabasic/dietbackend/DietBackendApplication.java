df040d283eb17bc1e136c2305ff52a5c10bed365
kittae <jinkitea@naver.com>
2024-09-23 13:36:41 +0900

    Add setting

---
+++ backend/Diet-Backend/src/main/java/jpabasic/dietbackend/DietBackendApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.dietbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietBackendApplication.class, args);
+	}
+
+}
